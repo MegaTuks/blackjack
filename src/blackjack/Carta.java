8ef921225cda8f2958782f63b9b688cd20fd9b33
juan__000 <juan__000@THECHOSEN>
2014-11-07 12:07:13 -0600

    tiene clases

---
+++ src/blackjack/Carta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author juan__000
+ */
+public class Carta {
+    private Mazo mazo;
+    private Mano mano;
+    private char palo;
+    private int valor;
+    public char getPalo(){return palo;}
+    public int getValor(){return valor;}
+}
---
+++ src/blackjack/Dealer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author juan__000
+ */
+public class Dealer {
+    private Mano _mano;
+    private Carta[] _carta;
+    public void getCarta(){return carta;}
+    public void hit(){return getCarta();}
+    public bool stand(){return true;}
+}
